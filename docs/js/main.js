418f616f5029e9739bc4b794e2e05651b62add32
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2019-07-24 14:49:04 +0200

    Split up SASS code

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,8 @@
 var menuTrigger = document.querySelector('.js-mobile-menu-trigger');
 
 menuTrigger.addEventListener('click', function() {
-    var mainMenu = document.querySelector('.js-main-menu');
-    var activeClass = 'main-menu--active';
+    var mainMenu = document.querySelector('.js-menu');
+    var activeClass = 'menu--active';
 
     if (mainMenu.classList.contains(activeClass)) {
         mainMenu.classList.remove(activeClass);
